bfd069c194077469e6bd6a27a837d8b47bb6970d
elias.kruk <elias.kruk@gmail.com>
2015-09-25 11:35:31 -0300

    videos

--- includes/functions.js
+++ includes/functions.js
@@ -1,13 +1,12 @@
 function eventListener() {
+    var videoPlayer = document.getElementById("videoPlayer");
+    
     var hm = $(window).height() / 3.15;
     $('.maq-content').height(hm);
 
-    $("#maquina").on("pagebeforeshow", function (event, ui) {
-        $('#maquina .ui-content').css("visibility", "hidden");
-    });
-
     $("#maquina").on("pageshow", function (event, ui) {
         var hc = ($(window).height() - $('#maquina .ui-header').height()) * 0.9;
+        mylog("HC = " + hc + " ---- WH = " + $(window).height() + " ---- HEADER = " + $('#maquina .ui-header').height());
         $('#maquina .ui-content').height(hc);
         $('#maquina .ui-content').css("visibility", "visible");
 
@@ -19,29 +18,31 @@ function eventListener() {
             prevButton: '.swiper-button-prev'
         });
     });
+
+    $("#videos").on("pageshow", function (event, ui) {
+        var hc = ($(window).height() - $('#videos .ui-header').height()) * 0.9;
+        mylog("HC = " + hc + " ---- WH = " + $(window).height() + " ---- HEADER = " + $('#videos .ui-header').height());
+        $('#videos .ui-content').height(hc);
+        $('#videos .ui-content').css("visibility", "visible");
+    });
+    
+    $(".lista-videos a").click(function(e){
+        e.preventDefault();
+        
+        videoPlayer.src = $(this).attr("href");
+        videoPlayer.poster = $(this).find("img").attr("src");
+        
+        return false;
+    });
+    
+    $(".back").click(function(){
+        videoPlayer.pause();
+    });
 }
 
 function videos() {
-    var basepath_ = cordova.file.dataDirectory + 'workout/';
-    var apppath_ = cordova.file.applicationDirectory;
-    alert('base: ' + basepath_);
-    alert('app: ' + apppath_);
-
-
-//    var fileTransfer = new FileTransfer();
-//    var uri = cordova.file.applicationDirectory + 'www/' + workouts[curWorkout].exercices[curEx].video;
-//    fileTransfer.download(
-//            uri,
-//            cordova.file.dataDirectory + workouts[curWorkout].exercices[curEx].video,
-//            function (entry) {
-//                console.log("download complete: " + entry.fullPath);
-//                videoPlayer.src = cordova.file.dataDirectory + entry.fullPath;
-//                console.log("videoPlayer.src: " + videoPlayer.src);
-//            },
-//            function (error) {
-//                alert("download error source " + error.source + " (code: " + error.code + ")");
-//            }
-//    );
+    var apppath_ = cordova.file.externalRootDirectory;
+    alert('base: ' + apppath_);
 }
 function openPopup(msg) {
     alert(msg);
