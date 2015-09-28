194806b9c391cc408466170e74112b47d14a280c
elias.kruk <elias.kruk@gmail.com>
2015-09-28 17:24:26 -0300

    sss

--- includes/functions.js
+++ includes/functions.js
@@ -55,12 +55,6 @@ function eventListener() {
     
 }
 
-window.resolveLocalFileSystemURI("file:///example.txt", function(fileEntry) {
-        console.log(fileEntry.name);
-    }, function(error) {
-        console.log(error.code);
-    });
-
 function videos() {
     var apppath_ = cordova.file.externalRootDirectory;
     //alert('SD PATH: ' + apppath_);
